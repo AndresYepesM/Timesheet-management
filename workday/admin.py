802279af1fa3cf2ee6dad770490ed87bba25dee8
AndresYepesM <aymoreno567@gmail.com>
2022-04-16 10:35:29 -0400

    Timecard model created

--- workday/admin.py
+++ workday/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-# from workday.models import *
+from workday.models import *
 # # Register your models here.
 
-# admin.site.register(Workday)
-# admin.site.register(Timesheet)
+admin.site.register(Timecard)
--- workday/models.py
+++ workday/models.py
@@ -1,30 +1,18 @@
-# from calendar import day_abbr
-# from tabnanny import verbose
-# from django.db import models
-# from django.contrib.auth.models import User
-# from user.models import Doctor
+from django.db import models
+from django.contrib.auth.models import User
+from user.models import Doctor, Location
 
-# # Create your models here.
+# Create your models here.
 
+class Timecard(models.Model):
+    workday = models.DateField(auto_now_add=True)
+    clock_in = models.TimeField()
+    clock_out = models.TimeField(null=True, blank=True)
+    employee = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    zone = models.ForeignKey(Location, on_delete=models.CASCADE)
 
-# class Workday(models.Model):
-#     day = models.DateField(auto_now_add=True)
-#     clock_in = models.TimeField()
-#     clock_out = models.TimeField(null=True, blank=True)
-
-#     class Meta:
-#         verbose_name_plural = 'Work day'
-
-#     def __str__(self):
-#         return f'{self.day} clock in: {self.clock_in}, clock out: {self.clock_out}'
-
-
-# class Timesheet(models.Model):
-#     employee = models.OneToOneField(Doctor, on_delete=models.CASCADE)
-#     record = models.ForeignKey(Workday, on_delete=models.CASCADE)
-
-#     class Meta:
-#         verbose_name_plural = 'Timesheet records'
-
-#     def __str__(self):
-#         return f'{self.id}.) {self.doctor.user.first_name} {self.doctor.user.last_name}'
+    class Meta:
+        verbose_name_plural = 'Timecards'
+    
+    def __str__(self):
+        return f'{self.workday} {self.employee.user.first_name}'
